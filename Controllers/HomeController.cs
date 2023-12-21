1e27f7a52bbfe60b0781c512915dacf065d61ff3
NerminOuCode <nerminouesleti99@gmail.com>
2023-12-21 15:05:51 +0100

    Set Home , login , register pages with mysql

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using Forumy.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Drawing.Printing;
+
+namespace Forumy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            // Handle user login and authentication using MySQL connection
+            if (Auth(user.Username, user.Password))
+            {
+                TempData["SuccessMessage"] = "Login successful!";
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Authentication failed, return to the login page with an error message
+                ModelState.AddModelError(string.Empty, "Invalid username or password");
+                return View(user);
+            }
+        }
+
+        private bool Auth(string username, string password)
+        {
+            using (MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM users WHERE Username = @Username AND Password = @Password";
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        return reader.HasRows;
+                    }
+                }
+            }
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            // Handle user login and authentication using MySQL connection
+            if (RegisterUser(user))
+            {
+                TempData["SuccessMessage"] = "Register successful!";
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Authentication failed, return to the login page with an error message
+                TempData["FailedMessage"] = "Register Failed , Check Your Information!";
+                return View(user);
+            }
+        }
+        private bool RegisterUser(User user)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Users (Username,Email , Password) VALUES (@Username,@Email, @Password)";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", user.Username);
+                        cmd.Parameters.AddWithValue("@Email", user.Email);
+                        cmd.Parameters.AddWithValue("@Password", user.Password);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed
+                return false;
+            }
+        
+    }
+    
+    public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
